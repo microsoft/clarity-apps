b1ab46e1e53badadc3f9de368f02f05f21bdacc4
Amr Alaa <amralaa@microsoft.com>
2024-09-02 13:17:00 +0300

    Versio 2.11.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clarity",
-            url: "https://clarityappsresourcesv2.blob.core.windows.net/ios-public/Clarity-2.1.0.xcframework.zip",
-            checksum: "38d7b6544fa76c1088bb9878dc1987c0d0494915f92d0145f4870914afd59230"
+            url: "https://clarityappsresourcesv2.blob.core.windows.net/ios-public/Clarity-2.11.0.xcframework.zip",
+            checksum: "617e9221e45b760454044d98b22e4bd0e9341b1aeaa0c53aa1e752a1d7e8714f"
         ),
     ]
 )
