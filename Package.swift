29af6c8d8b3f13ad38ad3717ea092bb4621eeed7
Amr Alaa <amralaa@microsoft.com>
2025-02-13 16:15:09 +0200

    Fix URL type

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clarity",
-            url: "https:///www.clarity.ms/apps/resources/ios/Clarity-1.0.3.xcframework.zip",
+            url: "https://www.clarity.ms/apps/resources/ios/Clarity-1.0.3.xcframework.zip",
             checksum: "1ae6f57d27e018b5ed9d1c2772ce7d8313cac6d988caa91d855bc6092d6f6fd0"
         ),
     ]
