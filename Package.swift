0898c9b3f8fc94b8c0ad9d228ad89a7b746495f0
dev-ops agent <clarityappsdeployer@microsoft.com>
2025-02-16 11:19:50 +0000

    Update target url and checksum in Package.swift to version 3.0.5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clarity",
-            url: "https://clarityappsresourcesv2.blob.core.windows.net/ios-public/Clarity-3.0.4.xcframework.zip",
-            checksum: "0aa9e0bf78296d13bcb5a7be778605c29b98f494d2353341c12f60967ef3c1ea"
+            url: "https://www.clarity.ms/apps/resources/ios/Clarity-3.0.5.xcframework.zip",
+            checksum: "de839c34a31645c399ba323cf0723087228fe2808431103a7120a3464f722bde"
         ),
     ]
 )
