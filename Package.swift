ab32fe13dd4b8d878728cb46beb27491779f62a5
Amr Alaa <amralaa@microsoft.com>
2026-04-16 17:23:44 +0200

    Update target url and checksum in Package.swift to version 3.5.1 (#262)
    Co-authored-by: dev-ops agent <clarityappsdeployer@microsoft.com>

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Clarity",
-            url: "https://www.clarity.ms/apps/resources/ios/Clarity-3.4.4.xcframework.zip",
-            checksum: "b42701d987d4645f242052cd37de5e3e97f7eb0f2b7b0eaf34425890fe8b2d13"
+            url: "https://www.clarity.ms/apps/resources/ios/Clarity-3.5.1.xcframework.zip",
+            checksum: "ac15f80647009189cbe67cd5c460e60409b601610330a33772a8b02fc827f9dc"
         ),
     ]
 )
